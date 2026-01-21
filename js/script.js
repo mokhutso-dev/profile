1e6dbcd6042e7104de882a84cbe941374d3ffb34
Mokhutso <manaswemahlako@gmail.com>
2026-01-21 09:07:34 +0200

    updated resume

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-    strings: ["Software Engineer", "DevOps Engineer"], 
+    strings: ["Software Engineer", "SRE Engineer"], 
     typeSpeed:60,
     backSpeed:100,
     backDelay:1000,
